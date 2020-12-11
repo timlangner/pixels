de096f41e2ff94438986811d3981292a9f01bf77
Tim <tim.langner@tobit.com>
2020-12-11 11:39:03 +0100

    :sparkles: Add states for panel options

--- src/components/editor/Editor.jsx
+++ src/components/editor/Editor.jsx
@@ -2,23 +2,57 @@ import React, { useState } from 'react';
 import './editor.scss';
 
 const Editor = () => {
+    const [panelWidth, setPanelWidth] = useState(16)
+    const [panelHeight, setPanelHeight] = useState(16)
+    const [hideOptions, setHideOptions] = useState(false)
+    const [hideDrawingPanel, setHideDrawingPanel] = useState(true)
+    const [buttonText, setButtonText] = useState("start drawing")
+    const [selectedColor, setColor] = useState("#f44336")
+
+    function initializeDrawingPanel() {
+        setHideOptions(!hideOptions)
+        setHideDrawingPanel(!hideDrawingPanel)
+
+        buttonText === "start drawing"
+            ? setButtonText("reset")
+            : setButtonText("start drawing")
+    }
+
     return (
         <div id="editor">
             <h1>Pixel Editor</h1>
-            <h2>Enter Panel Dimensions</h2>
-
-            <div id="options">
-                <div className="option">
-                    <input type="number" className="panelInput" />
-                    <span>Width</span>
+            {hideDrawingPanel && <h2>Enter Panel Dimensions</h2>}
+            {hideDrawingPanel && (
+                <div id="options">
+                    <div className="option">
+                        <input
+                            type="number"
+                            className="panelInput"
+                            defaultValue={panelWidth}
+                            onChange={e => {
+                                setPanelWidth(e.target.value)
+                            }}
+                        />
+                        <span>Width</span>
+                    </div>
+                    <div className="option">
+                        <input
+                            type="number"
+                            className="panelInput"
+                            defaultValue={panelHeight}
+                            onChange={e => {
+                                setPanelHeight(e.target.value)
+                            }}
+                        />
+                        <span>Height</span>
+                    </div>
                 </div>
-                <div className="option">
-                    <input type="number" className="panelInput" />
-                    <span>Height</span>
-                </div>
-            </div>
+            )}
+            <button onClick={initializeDrawingPanel} className="button">
+                {buttonText}
+            </button>
         </div>
-    );
+    )
 };
 
 export default Editor;
